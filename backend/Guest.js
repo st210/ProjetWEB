c83ffcfab6423211e90807ad9b622ea995791a3a
yanngodeau <yann.godeau@gmail.com>
2018-05-09 09:59:17 +0200

    Structure projet

--- backend/Guest.js
+++
@@ -1,9 +0,0 @@
-// Constructeur pour un Participant (accompagnateur/invité)
-// à compléter
-function Guest(id, name, firstname, mail, phone){
-    this.id = id;
-    this.name = name;
-    this.firstname = firstname;
-    this.mail = mail;
-    this.phone = phone;
-}
