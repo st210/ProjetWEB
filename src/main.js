a9348be59ff22e69b29a4ddb4d13dbe90543af9a
yanngodeau <yann.godeau@gmail.com>
2018-05-09 11:16:38 +0200

    Better project structure

---
+++ src/main.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var gestionEvent = require('.gestionEvent/');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('App listening on port 3000');
+});
