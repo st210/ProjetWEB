a731fa305d6e767e1148e99706858a35ff64f34c
Estee <edesanctis@laposte.net>
2018-05-25 18:40:04 +0200

    allow access control methods get-post-put-delete

--- src/main.js
+++ src/main.js
@@ -31,6 +31,17 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use(function(req,res){
+    res.sendfile(__dirname + '/html/index.html')
+});
+
+//Enable CORS
+app.use(function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+ next();
+});
 
 
 /**** Entités ****/
