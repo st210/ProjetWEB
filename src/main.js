4452bec97455baf0a8ad8320b73c4a95648c7eb5
Estee <edesanctis@laposte.net>
2018-05-25 22:04:34 +0200

    PLS

--- src/main.js
+++ src/main.js
@@ -5,8 +5,7 @@ var port = 3000;
 var mongoose = require('mongoose');
 
 // Options recommandées par mLab pour une connexion à la base
-var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+var options = { keepAlive: 300000, connectTimeoutMS: 30000 };
 
 // URL de la base de données
 var urlmongo = "mongodb://admin:admin@ds235180.mlab.com:35180/bd-evenements";
@@ -28,20 +27,30 @@ var event = require('./Model/Event');
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
+//app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/css', express.static('View/assets/vendor/bootstrap/css/bootstrap.css'));
+app.use('/css', express.static('View/assets/vendor/metisMenu/metisMenu.min.css'));
+app.use('/css', express.static('View/assets/dist/css/sb-admin-2.css'));
+app.use('/css', express.static('View/assets/vendor/morrisjs/morris.css'));
+app.use('/css', express.static('View/assets/vendor/font-awesome/css/font-awesome.min.css'));
 app.use(function(req,res){
-    res.sendfile(__dirname + '/html/index.html')
+    res.sendFile(__dirname + '/View/index.html')
 });
 
-//Enable CORS
+var ObjectID = mongoose.ObjectID;
+var EVENT_COLLECTION = "events";
+
+//Activer CORS: permet request entre multiples hotes localhost
 app.use(function(req, res, next) {
  res.header("Access-Control-Allow-Origin", "*");
  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
  next();
 });
+
 
 
 /**** Entités ****/
@@ -95,11 +104,11 @@ app.post('/api', function(req,res){
         }
         res.send({message : 'L\'évènement a bien été enregistré!'});
       })
-})
+});
 
 
-
-app.get('/api/event/get/:idE', function (req, res){
+// find an event by id
+app.get('/api/event/:idE', function (req, res){
     var idE = req.params.idE;
     if (isNaN(idE)){
         res.status(400).json({error: `Event ${idE} invalide.`});
@@ -114,7 +123,21 @@ app.get('/api/event/get/:idE', function (req, res){
     }
 });
 
+// update an event by id
+//app.put('/api/event/get/:idE', function (req, res){});
+
+// delete an event by id
+//app.delete('/api/event/get/:idE', function (req, res){});
+
+// find all events
 app.get('/api/event', function (req, res){
+    db.collection(EVENT_COLLECTION).find({}).toArray(function(err, docs) {
+        if (err) {
+          handleError(res, err.message, "Failed to get events.");
+        } else {
+          res.status(200).json(docs);
+        }
+     });
     res.json({message: "Liste test event: ",
               idE: req.query.idE,
               acronym: req.query.acronym,
@@ -127,8 +150,28 @@ app.get('/api/event', function (req, res){
               typePart: req.query.typePart});
 });
 
+// create a new event
+app.post("/api/event", function(req, res) {
+  var newEvent = req.body;
+  newEvent.createDate = new Date();
+
+  if (!req.body.name) {
+    handleError(res, "Saisie invalide.", "Vous devez entrer un nom.", 400);
+  }
+
+  db.collection(EVENT_COLLECTION).insertOne(newEvent, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Erreur. Impossible de créer un nouvel évènement.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+
 // appel test url: http://localhost:3000/api/event?idE=1&acronym=L3&name=test&place=test&desc=test&dateDeb=test&dateFin=test&nbMax=2&typePart=test
-/*app.post('/api/event', function (req, res){
+/* ancien post: créé event et permet affichage json via get (lien au dessus)
+app.post('/api/event', function (req, res){
     var event = new Event();
     event.idE = req.body.idE;
     event.acronym = req.body.acronym;
